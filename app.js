81b75c3685166720eac0b0d18c46465ede2a0856
Revant-1 <118761487+Revant-1@users.noreply.github.com>
2023-04-11 19:36:09 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+//const request = require("request");
+const https = require("https");
+var request = require('superagent');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+
+// Route to serve the HTML file
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+  });
+  
+  // Start the server
+  app.listen(process.env.PORT || 3003, function() {
+    console.log('Server is running on port 3003');
+  });
+
+
+
+  var mailchimpInstance   = 'us17';
+  var listUniqueId        = '8a1ee774df';
+  var mailchimpApiKey     = '42e5d6f028264c1260701d5ffe38a83a-us17';
+
+app.post('/', function (req, res) {
+  const firstName = req.body.Fname;
+  const lastName = req.body.Lname;
+  const email = req.body.email;
+
+  request
+      .post('https://' + mailchimpInstance + '.api.mailchimp.com/3.0/lists/' + listUniqueId + '/members/')
+      .set('Content-Type', 'application/json;charset=utf-8')
+      .set('Authorization', 'Basic ' + new Buffer('any:' + mailchimpApiKey ).toString('base64'))
+      .send({
+        'email_address': req.body.email,
+        'status': 'subscribed',
+        'merge_fields': {
+          'FNAME': req.body.firstName,
+          'LNAME': req.body.lastName
+        }
+      })
+          .end(function(err, response) {
+            if (response.status < 300 || (response.status === 400 && response.body.title === "Member Exists")) {
+              res.sendFile(__dirname + '/success.html');
+            } else {
+              res.sendFile(__dirname + '/failure.html');
+            }
+        });
+});
+
+app.post("/failure" ,function (req, res) {
+  res.redirect("/")
+});
+
+
+/*
+// install npm i body-parser and connect api data with html names, of websites
+
+  app.post("/", function(req, res){
+    const firstName = req.body.Fname;
+    const lastName = req.body.Lname;
+    const email = req.body.email;
+
+const data ={
+  member : [
+    {
+        
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+    }
+  ]
+}
+
+const jsonData = JSON.stringify(data);
+
+console.log(jsonData);
+
+const url="https://us17.api.mailchimp.com/3.0/lists/8a1ee774df"; 
+    const options=
+        {
+            methods:"POST",
+            auth:"revant:42e5d6f028264c1260701d5ffe38a83a-us17"
+        }
+    const request = https.request(url,options,function(response)
+            {
+                response.on("data",function(data)
+                        {
+                            console.log(JSON.parse(data));
+                        })
+            })
+    request.write(jsonData);
+    request.end();
+
+
+   
+    
+  });
+*/
+
+
+    //API key
+    // 42e5d6f028264c1260701d5ffe38a83a-us17
+
+   // unique ID
+ //   8a1ee774df
