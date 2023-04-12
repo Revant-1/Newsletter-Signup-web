07c171595ecc6cd46f4d91afe5906e2b3dd99593
Revant-1 <118761487+Revant-1@users.noreply.github.com>
2023-04-12 18:24:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
   });
   
   // Start the server
-  app.listen(process.env.PORT || 3003, function() {
+  app.listen(3003, function() {
     console.log('Server is running on port 3003');
   });
 
